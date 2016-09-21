534ff4552e09bd21a158abd30eaf43f9e52e7eb5
Thomas Burns <thomasburns@Thomass-MacBook-Pro.local>
2016-09-21 16:28:24 -0700

    CRUD 1st Attempt

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,32 @@
+class WikisController < ApplicationController
+
+  def index
+    @wikis = Wiki.all
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+      flash[:notice] = "Wiki has been saved successfully."
+      redirect_to @wiki
+    else
+      flash[:alert] = "There was an error saving the wiki. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+end
