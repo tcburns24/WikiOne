13ad680af634539debc5dbac1c4fe32bdb120504
Thomas Burns <thomasburns@Thomass-MacBook-Pro.local>
2016-09-23 14:57:45 -0700

    Edited failing tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,7 +58,8 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryGirl::Syntax::Methods
-  config.include Devise::TestHelpers, type: :controller
-  config.include Devise::TestHelpers, type: :view
+  config.include Devise::Test::ControllerHelpers
+  # config.include Devise::TestHelpers, type: :controller
+  # config.include Devise::TestHelpers, type: :view
 
 end
