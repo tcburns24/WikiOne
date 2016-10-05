f878ba7a226334fd600191aeab8d67978b61bf43
Tom <tcburns24@gmail.com>
2016-10-05 11:44:07 -0700

    error showing wikis on users/show.html.erb (#8)
    * error showing wikis on users/show.html.erb
    
    * Small fix to seeds
    
    * checked upgrading to premium

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,38 @@
+class ChargesController < ApplicationController
+  before_action :check_for_premium
+
+  def create
+    customer = Stripe::Customer.create(
+      email: current_user.email,
+      card: params[:stripeToken]
+    )
+
+    Stripe::Charge.create(
+      customer: customer.id,
+      amount: 75,
+      description: "Premium WikiOne Membership - #{current_user.email}",
+      currency: 'usd'
+    )
+
+    current_user.add_role :premium
+    flash[:notice] = "Congratulations on becoming a Premium WikiOne-er"
+    redirect_to profile_path
+
+    rescue Stripe::CardError => e
+      flash[:alert] = e.message
+      redirect_to new_charge_path
+  end
+
+  def new
+  end
+
+  private
+
+  def check_for_premium
+    if current_user.is_premium?
+      flash[:notice] = "You're already a premium member, no need to pay"
+      redirect_to profile_path
+    end
+  end
+
+end
