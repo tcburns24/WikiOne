e98d2df78801b6cfa53b2425c29df0afee50ddd7
Thomas Burns <thomasburns@Thomass-MacBook-Pro.local>
2016-10-21 15:58:09 -0700

    1st attempt-FriendlyId

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,6 +2,9 @@ class Wiki < ApplicationRecord
   belongs_to :user
   has_many :collaborators
 
+  extend FriendlyId
+  friendly_id :title use: :slugged
+
   def downgrade
     self.update(private: false)
   end
