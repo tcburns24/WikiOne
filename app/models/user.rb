e6a076a0cc5d40aba0dc0f8fa592bb6e655e2869
Thomas Burns <tcburns24@gmail.com>
2017-09-09 12:44:13 -0700

    confirmable removed from devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :wikis
